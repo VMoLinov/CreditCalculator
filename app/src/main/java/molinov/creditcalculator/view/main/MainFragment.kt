ad01a3639cafdb47fc931613d01b601d0dca3952
VMolinov <molinov@list.ru>
2021-06-24 18:54:53 +0300

    DatePicker

--- app/src/main/java/molinov/creditcalculator/view/main/MainFragment.kt
+++ app/src/main/java/molinov/creditcalculator/view/main/MainFragment.kt
@@ -1,16 +1,24 @@
 package molinov.creditcalculator.view.main
 
+import android.annotation.SuppressLint
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.Toast
+import com.google.android.material.datepicker.CalendarConstraints
+import com.google.android.material.datepicker.DateValidatorPointForward
+import com.google.android.material.datepicker.MaterialDatePicker
 import molinov.creditcalculator.R
 import molinov.creditcalculator.databinding.MainFragmentBinding
 import molinov.creditcalculator.view.schedule.ScheduleFragment
 import molinov.creditcalculator.viewmodel.MainViewModel
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainFragment : Fragment() {
 
@@ -29,15 +37,67 @@ class MainFragment : Fragment() {
         return binding.root
     }
 
+    @SuppressLint("ClickableViewAccessibility")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.scheduleBtn.setOnClickListener {
-            parentFragmentManager.apply {
-                beginTransaction()
-                    .replace(R.id.container, ScheduleFragment.newInstance())
-                    .addToBackStack("")
-                    .commit()
+        binding.firstPaymentField.setOnTouchListener { _, event ->
+            if (MotionEvent.ACTION_UP == event.action) {
+                editTextClicked()
             }
+            return@setOnTouchListener false
+        }
+        binding.scheduleBtn.setOnClickListener {
+            if (checkFields()) {
+                openSchedule()
+            } else {
+                Toast.makeText(context, "Заполните все поля", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    @SuppressLint("SimpleDateFormat")
+    private fun editTextClicked() {
+        val picker =
+            MaterialDatePicker.Builder.datePicker().setTitleText("Выберите дату")
+                .setCalendarConstraints(setConstraints()).setSelection(setDate()).build()
+        picker.addOnPositiveButtonClickListener {
+            val calendar = Calendar.getInstance(TimeZone.getTimeZone("UTC"))
+            calendar.timeInMillis = it
+            val formatter = SimpleDateFormat("dd.MM.yyyy")
+            binding.firstPaymentField.setText(formatter.format(calendar.time))
+        }
+        picker.show(this.parentFragmentManager, "DATE_PICKER")
+    }
+
+    private fun setConstraints(): CalendarConstraints {
+        return CalendarConstraints.Builder().setValidator(DateValidatorPointForward.now()).build()
+    }
+
+    private fun setDate(): Long {
+        val today = MaterialDatePicker.todayInUtcMilliseconds()
+        val calendar = Calendar.getInstance(TimeZone.getTimeZone("UTC"))
+        calendar.timeInMillis = today
+        calendar.add(Calendar.MONTH, 1)
+        return calendar.timeInMillis
+    }
+
+    private fun checkFields(): Boolean {
+        binding.apply {
+            if (firstPaymentField.text.isNullOrEmpty() || creditAmountField.text.isNullOrEmpty()
+                || loanTermField.text.isNullOrEmpty() || rateField.text.isNullOrEmpty()
+            ) {
+                return false
+            }
+            return true
+        }
+    }
+
+    private fun openSchedule() {
+        parentFragmentManager.apply {
+            beginTransaction()
+                .replace(R.id.container, ScheduleFragment.newInstance())
+                .addToBackStack("")
+                .commit()
         }
     }
 
@@ -48,6 +108,10 @@ class MainFragment : Fragment() {
         binding.creditType.setAdapter(adapter)
     }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
 
     companion object {
         fun newInstance() = MainFragment()
